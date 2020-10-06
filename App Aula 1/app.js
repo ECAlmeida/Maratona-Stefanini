13d158fb1920c53f988343efc9fb72207d6a16c0
Eder do Carmo Almeida <ederdoalmeida@hotmail.com>
2020-10-06 13:47:20 -0300

    Aula inicial, exemplo de exibição e de adição/subtração

---
+++ App Aula 1/app.js
@@ -0,0 +1,13 @@
+(function (){
+  "use strict";
+
+  angular
+  .module('app1',[])
+  .controller('controlador', function($scope){
+    
+    $scope.nome = 'Eder'
+    $scope.contador = 10
+
+
+  })
+})();
